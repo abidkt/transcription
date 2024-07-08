c86ea4fab9e73adee3b7aa8781347dd56d85e3e6
Zainul Abideen K T <kt.abid@gmail.com>
2024-07-08 14:59:29 +0530

    script change

--- run.sh
+++ run.sh
@@ -1,28 +1,15 @@
 #!/bin/bash
 
-# Display initial message
-echo "This script will start Transcription Stream."
-echo "Note: Script checks for latest llama3 model (if running ts-gpt in docker-compose.yml) then connects you to the logs"
-echo -n "Do you want to continue? (y/n): "
+# Start the docker-compose services
+echo "Starting services with docker-compose..."
+docker-compose up --detach
 
-# Read user input
-read answer
+# If running ts-gpt ollama container, enable this
+# Get the model installed on ts-gpt (requires curl)
+echo "Downloading ollama model"
+curl -X POST http://localhost:11434/api/pull -d '{"name": "llama3"}'
+curl -X POST http://localhost:11434/api/pull -d '{"name": "gemma2"}'
 
-# Check if the user input is 'y' or 'Y'
-if [ "$answer" != "${answer#[Yy]}" ] ;then
-    # Start the docker-compose services
-    echo "Starting services with docker-compose..."
-    docker-compose up --detach
-
-    # If running ts-gpt ollama container, enable this
-    # Get the model installed on ts-gpt (requires curl)
-    echo "Downloading ollama model"
-    curl -X POST http://localhost:11434/api/pull -d '{"name": "llama3"}'
-    curl -X POST http://localhost:11434/api/pull -d '{"name": "gemma2"}'
-
-    # Re-attach to compose logs
-    echo "Re-attaching to console logs"
-    docker-compose logs -f
-else
-    echo "Installation canceled by the user."
-fi
+# Re-attach to compose logs
+echo "Re-attaching to console logs"
+docker-compose logs -f
