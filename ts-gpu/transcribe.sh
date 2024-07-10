c4f5e5cdd1f47cb7c98a06e78b5b7d054e27ca3e
Zainul Abideen K T <kt.abid@gmail.com>
2024-07-10 17:03:14 +0530

    changes

--- ts-gpu/transcribe.sh
+++ ts-gpu/transcribe.sh
@@ -80,9 +80,8 @@ for sub_dir in "${sub_dirs[@]}"; do
         if [ -e "$incoming_dir$data_file" ]
         then
             mv "$incoming_dir$data_file"  "$transcribed_dir$row_id/"
+            python3 notify.py --path "$transcribed_dir$row_id/"
         fi
-
-        python3 /root/scripts/notify.py --path "$transcribed_dir$row_id/"
 
         rm -rf $incoming_dir
     done
