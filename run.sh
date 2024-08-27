fb4be5c3631829980fb184772a04116b3cb2592b
Zainul Abideen K T <kt.abid@gmail.com>
2024-08-27 16:05:21 +0530

    fixes

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ docker-compose up --detach
 # If running ts-gpt ollama container, enable this
 # Get the model installed on ts-gpt (requires curl)
 echo "Downloading ollama model"
-curl -X POST http://localhost:11435/api/pull -d '{"name": "llama3"}'
-curl -X POST http://localhost:11435/api/pull -d '{"name": "gemma2"}'
+curl -X POST http://localhost:11434/api/pull -d '{"name": "llama3"}'
+curl -X POST http://localhost:11434/api/pull -d '{"name": "gemma2"}'
 
 # Re-attach to compose logs
 echo "Re-attaching to console logs"
